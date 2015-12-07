c914a40af454c7e7f6c59bb457d5f66d2bd416b6
Bernhard Reiter <ockham@raz.or.at>
2015-12-07 23:50:43 +0100

    Remove CurrentThemeStore, update test

---
+++ shared/lib/themes/test/current-theme.js
@@ -0,0 +1,36 @@
+import { expect } from 'chai';
+
+import { createStore } from 'redux';
+
+import ThemeConstants from '../constants';
+import { reducer } from '../reducers/current-theme';
+
+describe( 'CurrentThemeStore', function() {
+	const SITE = { ID: 77203074 }; // dummy id
+
+	const actionReceiveCurrentTheme = {
+		type: ThemeConstants.RECEIVE_CURRENT_THEME,
+		themeId: 'twentyfifteen',
+		themeName: 'Twenty Fifteen',
+		site: SITE
+	};
+
+	let store;
+
+	beforeEach( function() {
+		store = createStore( reducer );
+	} );
+
+	describe( 'get()', function() {
+		beforeEach( function() {
+			store.dispatch( actionReceiveCurrentTheme );
+		} );
+
+		it( 'returns the current theme for the supplied site id', function() {
+			const currentTheme = store.getState().get( 'currentThemes' ).get( SITE.ID );
+
+			expect( currentTheme.id ).to.equal( 'twentyfifteen' );
+			expect( currentTheme.name ).to.equal( 'Twenty Fifteen' );
+		} );
+	} );
+} );
