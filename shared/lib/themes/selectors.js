081567c24d2eafea21e9253126901018babf34c2
Bernhard Reiter <ockham@raz.or.at>
2015-12-07 21:58:19 +0100

    components/data/activating-theme: Use Redux store and actions instead of Flux ones

--- shared/lib/themes/selectors.js
+++ shared/lib/themes/selectors.js
@@ -57,6 +57,14 @@ export function getCurrentTheme( state, siteId ) {
 	return state.themes.currentTheme.get( 'currentThemes' ).get( siteId );
 }
 
+export function isActivating( state ) {
+	return state.themes.currentTheme.get( 'isActivating' );
+}
+
+export function hasActivated( state ) {
+	return state.themes.currentTheme.get( 'hasActivated' );
+}
+
 function matches( theme, rawSearch ) {
 	const search = rawSearch.toLowerCase().trim();
 
