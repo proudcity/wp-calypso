259d0b1ebed0194b05c36d0a58eba6808904cc77
Bernhard Reiter <ockham@raz.or.at>
2015-12-10 22:40:51 +0100

    Remove ThemesListStore, update test
    Had to remove the Jetpack test since filtering is no longer done in
    the reducer but in the themes-list-fetcher data component (using the
    getFilteredThemes() selector) now.

--- shared/lib/themes/test/themes-list-store.js
+++ shared/lib/themes/test/themes-list-store.js
@@ -1,100 +1,100 @@
-var assert = require( 'chai' ).assert,
-	sinon = require( 'sinon' ),
-	rewire = require( 'rewire' );
+import { assert } from 'chai';
 
-var ThemeConstants = require( 'lib/themes/constants' );
+import { createStore } from 'redux';
 
-describe( 'ThemesListStore', function() {
-	var ThemesListStore, registeredCallback,
-		actionReceiveThemes = {
-			action: {
-				type: ThemeConstants.RECEIVE_THEMES,
-				queryParams: {
-					id: 1
-				},
-				themes: [
-					{ id: 'bold-news', name: 'Bold News' },
-					{ id: 'picard', name: 'Picard' },
-					{ id: 'picard', name: 'Picard' }
-				]
+import ThemeConstants from '../constants';
+import { reducer } from '../reducers/themes-list';
+
+describe( 'themes-list', () => {
+	const actionReceiveThemes = {
+		type: ThemeConstants.RECEIVE_THEMES,
+		queryParams: {
+			id: 1
 		},
-	},
-	actionReceiveMoreThemes = {
-		action: {
-			type: ThemeConstants.RECEIVE_THEMES,
-			queryParams: {
-				id: 1
-			},
-			themes: [
-				{ id: 'picard', name: 'Picard' },
-				{ id: 'hue', name: 'Hue' }
-			]
-		}
-	},
-	actionReceiveEvenMoreThemes = {
-		action: {
-			type: ThemeConstants.RECEIVE_THEMES,
-			queryParams: {
-				id: 2
-			},
-			themes: [
-				{ id: '2014', name: 'Twenty Fourteen' },
-				{ id: 'cheese', name: 'Cheese' }
-			]
-		}
-	},
-	actionQueryThemes = {
-		action: {
-			type: ThemeConstants.QUERY_THEMES,
-			params: {
-				search: 'picard',
-				perPage: 50,
-				page: 25
-			}
-		}
-	},
-	actionQueryAnotherTheme = {
-		action: {
-			type: ThemeConstants.QUERY_THEMES,
-			params: {
-				search: 'worf',
-				perPage: 50,
-				page: 25
-			}
-		}
-	},
-	actionIncrementPage = {
-		action: {
-			type: ThemeConstants.INCREMENT_THEMES_PAGE
+		themes: [
+			{ id: 'bold-news', name: 'Bold News' },
+			{ id: 'picard', name: 'Picard' },
+			{ id: 'picard', name: 'Picard' }
+		]
+	};
+
+	const actionReceiveMoreThemes = {
+		type: ThemeConstants.RECEIVE_THEMES,
+		queryParams: {
+			id: 1
+		},
+		themes: [
+			{ id: 'picard', name: 'Picard' },
+			{ id: 'hue', name: 'Hue' }
+		]
+	};
+
+	const actionReceiveEvenMoreThemes = {
+		type: ThemeConstants.RECEIVE_THEMES,
+		queryParams: {
+			id: 2
+		},
+		themes: [
+			{ id: '2014', name: 'Twenty Fourteen' },
+			{ id: 'cheese', name: 'Cheese' }
+		]
+	};
+
+	const actionQueryThemes = {
+		type: ThemeConstants.QUERY_THEMES,
+		params: {
+			search: 'picard',
+			perPage: 50,
+			page: 25
 		}
 	};
 
-	beforeEach( function() {
-		ThemesListStore = rewire( 'lib/themes/themes-list-store' );
-		registeredCallback = ThemesListStore.__get__( 'registeredCallback' );
-		ThemesListStore.__set__( 'Dispatcher', { waitFor: sinon.stub() } );
+	const actionQueryAnotherTheme = {
+		type: ThemeConstants.QUERY_THEMES,
+		params: {
+			search: 'worf',
+			perPage: 50,
+			page: 25
+		}
+	};
+
+	const actionIncrementPage = {
+		type: ThemeConstants.INCREMENT_THEMES_PAGE
+	};
+
+	let store;
+
+	function getThemesList() {
+		return store.getState().get( 'list' );
+	}
+
+	function getQueryParams() {
+		return store.getState().get( 'query' ).toObject();
+	}
+
+	beforeEach( () => {
+		store = createStore( reducer );
 	} );
 
-	describe( 'get()', function() {
-		beforeEach( function() {
-			registeredCallback( actionQueryThemes );
-			registeredCallback( actionReceiveThemes );
+	describe( 'get()', () => {
+		beforeEach( () => {
+			store.dispatch( actionQueryThemes );
+			store.dispatch( actionReceiveThemes );
 		} );
 
-		it( 'returns all themes', function() {
-			var themes = ThemesListStore.get();
-
+		it( 'returns all themes', () => {
+			const themes = getThemesList();
 			assert( themes.length === 2, 'Wrong number of themes' );
 		} );
 	} );
 
-	describe( 'query()', function() {
-		beforeEach( function() {
-			registeredCallback( actionQueryThemes );
+	describe( 'query()', () => {
+		beforeEach( () => {
+			store.dispatch( actionQueryThemes );
 		} );
 
-		it( 'sets the query paramaters', function() {
-			var params = ThemesListStore.getQueryParams();
+		it( 'sets the query paramaters', () => {
+			var params = getQueryParams();
 
 			assert( params.search === 'picard', 'Wrong param' );
 			assert( params.perPage === 50, 'Wrong param' );
@@ -102,72 +102,44 @@ describe( 'ThemesListStore', function() {
 		} );
 	} );
 
-	describe( 'getQueryParams()', function() {
-		context( 'when THEMES_INCREMENT_PAGE is received', function() {
-			beforeEach( function() {
-				registeredCallback( actionIncrementPage );
+	describe( 'getQueryParams()', () => {
+		context( 'when THEMES_INCREMENT_PAGE is received', () => {
+			beforeEach( () => {
+				store.dispatch( actionIncrementPage );
 			} );
 
-			it( 'increments the page number', function() {
-				var params = ThemesListStore.getQueryParams();
+			it( 'increments the page number', () => {
+				var params = getQueryParams();
 
 				assert( params.page === 1 );
 			} );
 		} );
 	} );
 
-	context( 'when THEMES_RECEIVE is received', function() {
-		beforeEach( function() {
-			registeredCallback( actionQueryThemes );
-			registeredCallback( actionReceiveThemes );
+	context( 'when THEMES_RECEIVE is received', () => {
+		beforeEach( () => {
+			store.dispatch( actionQueryThemes );
+			store.dispatch( actionReceiveThemes );
 		} );
 
-		it( 'removes duplicates', function() {
-			registeredCallback( actionReceiveMoreThemes );
+		it( 'removes duplicates', () => {
+			store.dispatch( actionReceiveMoreThemes );
 
-			assert( ThemesListStore.get().length === 3, 'incorrect number of themes' );
+			assert( getThemesList().length === 3, 'incorrect number of themes' );
 		} );
 	} );
 
 	context( 'when two THEMES_RECEIVE are received out of order', () => {
 		beforeEach( () => {
-			registeredCallback( actionQueryThemes ); // first query, query ID of 1
-			registeredCallback( actionQueryAnotherTheme ); // second query, ID 2
-			registeredCallback( actionReceiveEvenMoreThemes ); // receive themes from second query (ID 2)
-			registeredCallback( actionReceiveThemes ); // receive themes from first query (ID 1)
+			store.dispatch( actionQueryThemes ); // first query, query ID of 1
+			store.dispatch( actionQueryAnotherTheme ); // second query, ID 2
+			store.dispatch( actionReceiveEvenMoreThemes ); // receive themes from second query (ID 2)
+			store.dispatch( actionReceiveThemes ); // receive themes from first query (ID 1)
 		} );
 
 		it( 'only takes into account the last query results', () => {
-			assert( ThemesListStore.get().length === 2 );
-			assert( ThemesListStore.get()[0] === '2014' );
-		} );
-	} );
-
-	context( 'when on a Jetpack site', function() {
-		context( 'when THEMES_RECEIVE is received', function() {
-			let rewireRevert;
-
-			beforeEach( function() {
-				rewireRevert = ThemesListStore.__set__( {
-					ThemesLastQueryStore: {
-						isJetpack: () => true,
-						getParams: () => ( { search: 'picard' } ),
-					},
-					ThemesStore: {
-						get: () => actionReceiveMoreThemes.action.themes,
-					}
-				} );
-			} );
-
-			it( 'filters available themes according to search', function() {
-				registeredCallback( actionReceiveMoreThemes );
-				assert.equal( ThemesListStore.get().length, 1 );
-				assert.equal( ThemesListStore.get()[0], 'picard' );
-			} );
-
-			afterEach( function() {
-				rewireRevert();
-			} );
+			assert( getThemesList().length === 2 );
+			assert( getThemesList()[0] === '2014' );
 		} );
 	} );
 } );
