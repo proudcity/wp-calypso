4ff3acfbff93a14b511d8e348b37204162f59396
Bernhard Reiter <ockham@raz.or.at>
2015-12-10 22:49:24 +0100

    Remove obsolete getParams() selector

--- shared/lib/themes/selectors.js
+++ shared/lib/themes/selectors.js
@@ -7,10 +7,6 @@ export function isJetpack( state ) {
 	return state.themes.themesLastQuery.get( 'isJetpack' );
 }
 
-export function getParams( state ) {
-	return state.themes.themesLastQuery.get( 'lastParams' ) || {};
-}
-
 export function hasSiteChanged( state ) {
 	return state.themes.themesLastQuery.get( 'previousSiteId' ) !==
 		state.themes.themesLastQuery.get( 'currentSiteId' );
